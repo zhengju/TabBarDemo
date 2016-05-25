fb2ee5fa0723aaafc121a1ebc974604c2a44ab3c
zhengju <zhengju@zhengju.local>
2016-05-25 11:34:03 +0800

    Initial Commit

---
+++ TabBar_Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TabBar_Demo
+//
+//  Created by zhengju on 16/5/25.
+//  Copyright © 2016年 zhengju. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
